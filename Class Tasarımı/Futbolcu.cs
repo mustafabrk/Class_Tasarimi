5873d14800c1f0d67b9af181bb2109fe8d0b83fa
mustafabrk <mustafaburaksert@hotmail.com>
2021-04-26 19:22:46 +0300

    Class Tasarımı
    Nesneye Dayalı Programlama dersinin vizesi için hazırladığım 5 değişkenli ve kapsülleme ile dışarıdan erişime kapalı olan class tasarımının nesne üzerinde uygulanması.

---
+++ Class Tasarımı/Futbolcu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev4
+{
+    class Futbolcu
+    {
+        private string ad, soyad, mevki, takim;
+        private int yas;
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public string Soyad
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+        public string Mevki
+        {
+            get { return mevki; }
+            set { mevki = value; }
+        }
+        public int Yas
+        {
+            get { return yas; }
+            set { yas = value; }
+        }
+        public string Takim
+        {
+            get { return takim; }
+            set { takim = value; }
+        }
+        public string Bilgi()
+        {
+            return this.ad + this.soyad + " isimli " + this.yas + " yasindaki " + this.mevki + " oyuncusu "
+                + this.takim + "'nde forma giymektedir. ";
+        }
+    }
+
+}
---
+++ Class Tasarımı/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Odev4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Futbolcu futbolcu = new Futbolcu()
+            {
+                Ad = "Mustafa Burak",
+                Soyad = "Sert",
+                Mevki = "Libero",
+                Yas = 20,
+                Takim = "Bilecik Belediyesi"
+            };
+
+            futbolcu.Ad = "Mustafa Burak ";
+            futbolcu.Yas = 20;
+            futbolcu.Mevki = "Libero";
+            futbolcu.Takim = "Bilecik Belediyesi";
+            Console.WriteLine(futbolcu.Bilgi());
+            Console.ReadKey();
+
+        }
+    }
+}
